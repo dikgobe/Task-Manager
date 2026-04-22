e0045f9a92040fa076932bbec6f282aff2eebf7e
dikgobe <djmolepo@gmail.com>
2026-04-22 13:22:38 +0200

    defined app routes

--- frontend/Task-Manager/src/App.jsx
+++ frontend/Task-Manager/src/App.jsx
@@ -4,6 +4,15 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
+import Dashboard  from './pages/Admin/Dashboard';
+import Login from './pages/Auth/Login'
+import UserDashboard from './pages/User/UserDashboard';
+import MyTasks from './pages/User/MyTasks';
+import ManageUsers from './pages/Admin/ManageUsers';
+import ManageTasks from './pages/Admin/ManageTasks';
+import CreateTasks from './pages/Admin/CreateTasks';
+import ViewTaskDetails from './pages/User/ViewTaskDetails';
+import PrivateRoute from './components/PrivateRoute';
 
 const App = () => {
   return (
@@ -19,12 +28,14 @@ const App = () => {
             <Route path="/admin/dashboard" element={<Dashboard />} />
             <Route path="/admin/users" element={<ManageUsers />} />
             <Route path="/admin/tasks" element={<ManageTasks />} />
+            <Route path="/admin/create-tasks" element={<CreateTasks />} />
           </Route>
 
           {/* User Routes */}
           <Route element= {<PrivateRoute allowdRoles={['user']} />}>
-            <Route path="/user/dashboard" element={<Dashboard />} />
+            <Route path="/user/dashboard" element={<UserDashboard />} />
             <Route path="/user/my-tasks" element={<MyTasks />} />
+            <Route path="/user/tasks-details:id" element={<ViewTaskDetails />} />
           </Route>
 
         </Routes>
